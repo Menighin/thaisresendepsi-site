33b23891b5a2a6a1df7838199753d4d937290713
Menighin <joao.menighin@gmail.com>
2022-07-08 23:16:31 -0300

    Aligning contacts

--- src/components/Contacts.vue
+++ src/components/Contacts.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="contacts">
-    <a href="https://api.whatsapp.com/send?phone=5531995169604" target="blank"><span class="mai-logo-whatsapp"></span></a>
-    <a href="https://www.instagram.com/thaisresendepsi/" target="blank"><span class="mai-logo-instagram"></span></a>
-    <a href="https://www.linkedin.com/in/thais-resende-836683101/" target="blank"><span class="mai-logo-linkedin"></span></a>
+    <div class="contact"><a href="https://api.whatsapp.com/send?phone=5531995169604" target="blank"><span class="mai-logo-whatsapp"></span></a></div>
+    <div class="contact"><a href="https://www.instagram.com/thaisresendepsi/" target="blank"><span class="mai-logo-instagram"></span></a></div>
+    <div class="contact"><a href="https://www.linkedin.com/in/thais-resende-836683101/" target="blank"><span class="mai-logo-linkedin"></span></a></div>
   </div>
 </template>
 
@@ -15,11 +15,19 @@ export default {
 <style lang="scss">
   .contacts {
     margin: 0 10px;
-    a {
-      border-radius: 100%;
-      padding: 10px;
+    .contact {
+      display: inline-block;
+      width: 32px;
+      height: 32px;
       margin: 0 2px 0;
-      text-decoration: none;
+      border-radius: 100%;
+      line-height: 32px;
+      text-align: center;
+
+      a {
+        text-decoration: none;
+        vertical-align: middle;
+      }
     }
   }
 </style>
